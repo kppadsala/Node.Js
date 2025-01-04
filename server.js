b6e074c7d5fbcce38cb841082b895516c82f285f
kppadsala <krunalpadsala30@gmail.com>
2025-01-04 22:08:20 +0530

    Create server

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-var a=10
-console.log("🚀 ~ a:", a)
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',function (req, res)  {
+    res.send('Hello World!');
+})
+app.listen(port, () => {
+console.log(`Server is running on port ${port}`);
+})
